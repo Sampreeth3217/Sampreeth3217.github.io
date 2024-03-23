c5db77760193bac24f5dc7fc9fa97ac6c2bfba28
Sampreeth3217 <127882606+Sampreeth3217@users.noreply.github.com>
2024-03-23 23:38:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function ToggleMenu()
-{
-    const menu = document.querySelector(".Menu-links");
-    const icon = document.querySelector(".Hamburger-icon");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
